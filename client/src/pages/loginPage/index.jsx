56a5aea51a3d9026985c330981d8aa9647c14182
FOR-TIMI <timiplayskeys@gmail.com>
2022-12-28 18:41:44 -0500

    fixed styles for register

--- client/src/pages/loginPage/index.jsx
+++ client/src/pages/loginPage/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import {
   Box,
   Button,
@@ -6,8 +6,14 @@ import {
   useMediaQuery,
   Typography,
   useTheme,
-  Container
+  Container,
+  Avatar,
+  IconButton
 } from "@mui/material";
+
+
+import Tooltip from '@mui/material/Tooltip';
+import { ConstructionOutlined, PhotoCamera } from '@mui/icons-material';
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -91,9 +97,15 @@ const Form = () => {
   const handleFormSubmit = async (values, onSubmitProps) => {
     // if (isLogin) await login(values, onSubmitProps);
     // if (isRegister) await register(values, onSubmitProps);
-
+  console.log("hooray")
     console.log({ values, onSubmitProps})
   };
+
+  const fileInputRef = useRef(null);
+
+  const handleFileSelection = () => {
+    fileInputRef.current.click();
+  }
 
   return (
    <Container maxWidth="sm">
@@ -141,6 +153,52 @@ const Form = () => {
           >
             {isRegister && (
               <>
+              <Box
+                  gridColumn="span 4"
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  align-items="center"
+                >
+                   <Avatar src="/default-avatar.jpg" alt="JS" style={{ width: '5rem', height: '5rem', marginLeft: '2rem' }}  />
+                    <IconButton onClick={handleFileSelection} style={{ position: 'relative', left: '-1.8rem', top: '1.5rem'}}>
+                      <Tooltip title="Add profile photo">
+                        <PhotoCamera sx={{ fontSize: '2rem'}}/>
+                      </Tooltip>
+                    </IconButton>
+                    <input
+                      ref={fileInputRef}
+                      type="file"
+                      style={{ display: 'none' }}
+                      accept=".jpg, .jpeg, .png"
+                    />
+                  {/* <Dropzone
+                    acceptedFiles=".jpg,.jpeg,.png"
+                    multiple={false}
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue("picture", acceptedFiles[0])
+                    }
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        p="1rem"
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                      >
+                        <input  />
+                        {!values.picture ? (
+                          <p>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.name}</Typography>
+                            <EditOutlinedIcon />
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone> */}
+                </Box>
                 <TextField
                   label="First Name"
                   onBlur={handleBlur}
@@ -185,39 +243,6 @@ const Form = () => {
                   helperText={touched.occupation && errors.occupation}
                   sx={{ gridColumn: "span 4" }}
                 />
-                <Box
-                  gridColumn="span 4"
-                  border={`1px solid ${palette.neutral.medium}`}
-                  borderRadius="5px"
-                  p="1rem"
-                >
-                  <Dropzone
-                    acceptedFiles=".jpg,.jpeg,.png"
-                    multiple={false}
-                    onDrop={(acceptedFiles) =>
-                      setFieldValue("picture", acceptedFiles[0])
-                    }
-                  >
-                    {({ getRootProps, getInputProps }) => (
-                      <Box
-                        {...getRootProps()}
-                        border={`2px dashed ${palette.primary.main}`}
-                        p="1rem"
-                        sx={{ "&:hover": { cursor: "pointer" } }}
-                      >
-                        <input {...getInputProps()} />
-                        {!values.picture ? (
-                          <p>Add Picture Here</p>
-                        ) : (
-                          <FlexBetween>
-                            <Typography>{values.picture.name}</Typography>
-                            <EditOutlinedIcon />
-                          </FlexBetween>
-                        )}
-                      </Box>
-                    )}
-                  </Dropzone>
-                </Box>
               </>
             )}
 
@@ -245,13 +270,13 @@ const Form = () => {
           </Box>
 
           {/* BUTTONS */}
-          <Box padding="0 1.5rem">
+          <Box padding="0 1.5rem 1.5rem 1.5rem" >
             <Button
               fullWidth
               type="submit"
               sx={{
                 m: "2rem 0",
-                p: "1rem",
+                p: "0.8rem",
                 backgroundColor: palette.primary.main,
                 color: palette.background.alt,
                 "&:hover": { color: palette.primary.main },
