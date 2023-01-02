3a976a7ad2aa69c143db21e8d1ea8a46d6cc6f3e
FOR-TIMI <timiplayskeys@gmail.com>
2023-01-01 19:44:05 -0500

    fixed build errors

--- client/src/pages/widgets/AdWidget.jsx
+++ client/src/pages/widgets/AdWidget.jsx
@@ -3,35 +3,34 @@ import FlexBetween from "../../components/CustomStyledComponents/FlexBetween";
 import WidgetWrapper from "../../components/CustomStyledComponents/WidgetWrapper";
 import InfoIcon from '@mui/icons-material/Info';
 import { useState, useEffect } from "react";
-import Skeleton from '@mui/material/Skeleton';
+
+const ads = [
+  {
+      url: "https://i.pinimg.com/736x/d6/3d/5f/d63d5f27d6e46a6918a26f36a5f31c0f.jpg",
+      name: "Nike",
+      text: "From the field to the streets, we've got you covered."
+  },
+  {
+      url: "https://1000logos.net/wp-content/uploads/2019/06/Adidas-Logo-1991.jpg",
+      name: "Adidas",
+      text: "Elevate your performance with our gear."
+  },
+  {
+      url: "https://i0.wp.com/cultedge.com/wp-content/uploads/2019/02/Champion-logo-001.jpg",
+      name: "Champion",
+      text: "Where champions shop"
+  }
+]
+
+const getRandomAd = () => {
+  return ads[Math.floor(Math.random() * ads.length)]
+}
 
 const AdWidget = () => {
     const { palette } = useTheme();
     const { dark, main, medium} = palette;
 
-    const ads = [
-        {
-            url: "https://i.pinimg.com/736x/d6/3d/5f/d63d5f27d6e46a6918a26f36a5f31c0f.jpg",
-            name: "Nike",
-            text: "From the field to the streets, we've got you covered."
-        },
-        {
-            url: "https://1000logos.net/wp-content/uploads/2019/06/Adidas-Logo-1991.jpg",
-            name: "Adidas",
-            text: "Elevate your performance with our gear."
-        },
-        {
-            url: "https://i0.wp.com/cultedge.com/wp-content/uploads/2019/02/Champion-logo-001.jpg",
-            name: "Champion",
-            text: "Where champions shop"
-        }
-    ]
-
     const [ad, setAd] = useState(ads[0]);
-    
-    const getRandomAd = () => {
-        return ads[Math.floor(Math.random() * ads.length)]
-    }
 
     useEffect(() => {
         setInterval(()=> {
