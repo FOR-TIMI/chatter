e53dea2e03e45487d76576690118f5f7cb670a44
FOR-TIMI <timiplayskeys@gmail.com>
2023-01-01 21:40:12 -0500

    first deploy to heroku

--- server/server.js
+++ server/server.js
@@ -37,7 +37,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname + '/public')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname + '/public')))
+
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname + '/public'));
+  });
+}
+
 
 
 
