192434773ceecee152209562b8b118a37a12161b
FOR-TIMI <timiplayskeys@gmail.com>
2023-01-16 07:18:55 -0500

    deploying to heroku

--- client/src/components/FormatLikes/index.jsx
+++ client/src/components/FormatLikes/index.jsx
@@ -1,11 +1,13 @@
 
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, useTheme} from '@mui/material';
 import { Link } from 'react-router-dom';
+
 
 function FormatLike({ users, isNonMobileScreens, otherLikes }) {
   const [username, setUsername] = useState(users[0]);
+  const { palette } = useTheme()
 
   const handleClick = (username) => {
     setUsername(username);
@@ -13,26 +15,39 @@ function FormatLike({ users, isNonMobileScreens, otherLikes }) {
 
   return (
     <Box component='span' display="inline">
-      {isNonMobileScreens
-        ? users
-            .slice(0, 2)
-            .map((user,index) => (
-              <Link
-                key={uuidv4()}
-                to={`/profile/${user}`}
-                onClick={() => handleClick(user)}
+      {users.length > 1 ? (
+        isNonMobileScreens
+          ? users
+              .slice(0, 2)
+              .map((user, index) => (
+                <Link
+                  key={uuidv4()}
+                  to={`/profile/${user}`}
+                  onClick={() => handleClick(user)}
+                  sx={{color: palette.neutral.dark}}
+                >
+                  {user}
+                  {index !== 1 ? ', ' : ' '}
+                </Link>
+              ))
+          :  <Link
+                 to={`/profile/${username}`}
+                 onClick={() => handleClick(username)}
+               sx={{color: palette.neutral.dark}}
+
               >
-                {user}
-                {index !== 1 ? ', ' : ' '}
+                 {username + " "}
               </Link>
-            ))
-        :  <Link
-               to={`/profile/${username}`}
-               onClick={() => handleClick(username)}
+      ) : users.length === 1 ? (
+           <Link
+                to={`/profile/${username}`}
+                onClick={() => handleClick(username)}
+                sx={{color: palette.neutral.dark}}
+
             >
-               {username + " "}
+               {username}
             </Link>
-      }
+      ):(<></>)}
       {otherLikes > 0 ? (
         <Typography color="textSecondary" display="inline">
           and {otherLikes} {otherLikes > 1 ? "others" : "other"} like this
@@ -53,4 +68,8 @@ function FormatLike({ users, isNonMobileScreens, otherLikes }) {
 }
 
 export default FormatLike;
+
+
+
+
 
