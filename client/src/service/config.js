4ca90dd14405446f5066bdff5c0dfe927984f08b
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-19 19:07:59 -0400

    staging

--- client/src/service/config.js
+++ client/src/service/config.js
@@ -6,10 +6,8 @@ const config = {
   SOCKET_URL:
     process.env.REACT_APP_ENV === "Development"
       ? "ws://localhost:8900/"
-      : "ws://nameless-basin-36851.herokuapp.com:" + process.env.PORT
+      : `ws://nameless-basin-36851.herokuapp.com:${process.env.PORT}`,
 };
 
 export const SERVER_URL = config.SERVER_URL;
-export const SOCKET_URL = config.SOCKET_URL; 
-
-
+export const SOCKET_URL = config.SOCKET_URL;
