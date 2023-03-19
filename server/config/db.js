29f6779a1dacf5a4ac1253c63549503e5a6bd5ec
FOR-TIMI <timiplayskeys@gmail.com>
2023-03-19 17:21:30 -0400

    deploying to heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const DB_URL = process.env.MONGODB_URI  || 'mongodb://127.0.0.1:27017/chatter';
+const DB_URL = process.env.MONGODB_URI;
 
 
 
